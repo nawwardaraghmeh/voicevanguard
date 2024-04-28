f6a7b032e9737ae5aac520e6d6f0f4d88a123801
nawwardaraghmeh <daraghmehnawwar@gmail.com>
2024-04-28 09:41:38 +0300

    fixed learnmore button in events page

--- vv/webpages/events.aspx.designer.cs
+++ vv/webpages/events.aspx.designer.cs
@@ -42,15 +42,6 @@ namespace vv.web_pages
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
-        /// Button6 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button6;
-
-        /// <summary>
         /// Button2 control.
         /// </summary>
         /// <remarks>
@@ -85,5 +76,14 @@ namespace vv.web_pages
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button Button5;
+
+        /// <summary>
+        /// Button6 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button6;
     }
 }
