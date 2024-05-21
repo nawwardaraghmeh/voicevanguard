61fbd70f42a4934e25b8802618619ef4f730a5ae
nawwardaraghmeh <daraghmehnawwar@gmail.com>
2024-05-21 19:00:06 +0300

    fixed update password code

--- vv/webpages/resetpasswordi.aspx.cs
+++ vv/webpages/resetpasswordi.aspx.cs
@@ -1,23 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Net.Mail;
 using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace vv.webpages
 {
-    public partial class resetpasswordi : System.Web.UI.Page
+    public partial class resetpasswordi : Page
     {
-
-        
         protected void Page_Load(object sender, EventArgs e)
         {
-
         }
 
         protected void cancelHlink_Click(object sender, EventArgs e)
@@ -32,7 +25,7 @@ namespace vv.webpages
             string userEmail = setEmailtxtbox.Text;
             bool emailSent = false;
 
-            if(userEmail != "")
+            if (userEmail != "")
             {
                 SendResetPasswordEmail(userEmail, resetUrl);
                 emailSent = true;
@@ -44,42 +37,12 @@ namespace vv.webpages
 
             if (emailSent)
             {
+                // Set the session variable before redirecting
+                Session["UserEmail"] = userEmail;
                 Response.Redirect("~/webpages/resetlink.aspx");
             }
-
-            Session["UserEmail"] = userEmail;
-
-
-            /* string email = setEmailtxtbox.Text;
-
-             if (doesEmailExist(email))
-             {
-                 string token = Guid.NewGuid().ToString();
-
-                 // Store the token in the database along with the user's email and timestamp
-                 StoreTokenInDatabase(email, token);
-
-                 // Send an email to the user's email address with a link that includes the token
-                 string resetLink = $"https://localhost:44313/webpages/resetpasswordii.aspx?token={HttpUtility.UrlEncode(token)}";
-                 bool emailSent = SendResetEmail(email, resetLink);
-
-                 if (emailSent)
-                 {
-                     // Redirect to a page indicating that the reset link has been sent
-                     Response.Redirect("~/webpages/resetlink.aspx");
-                 }
-                 else
-                 {
-                     errorlbl.Text = "Sending password reset link failed. Please try again.";
-                 }
-             }
-             else
-             {
-                 errorlbl.Text = "Email address not found";
-             }*/
         }
 
-       
         public string GetResetPasswordUrl(string token)
         {
             string baseUrl = "https://localhost:44313/webpages/resetpasswordii";
@@ -96,8 +59,8 @@ namespace vv.webpages
 
             var smtp = new SmtpClient
             {
-                Host = "smtp.gmail.com", 
-                Port = 587, 
+                Host = "smtp.gmail.com",
+                Port = 587,
                 EnableSsl = true,
                 DeliveryMethod = SmtpDeliveryMethod.Network,
                 UseDefaultCredentials = false,
@@ -113,74 +76,5 @@ namespace vv.webpages
                 smtp.Send(message);
             }
         }
-
-        /* private bool doesEmailExist(string email)
-         {
-             string connectionString = ConfigurationManager.ConnectionStrings["VoiceVanguardDB"].ConnectionString;
-             string query = "SELECT COUNT(*) FROM users WHERE email = @email";
-
-             using (SqlConnection connection = new SqlConnection(connectionString))
-             {
-                 SqlCommand command = new SqlCommand(query, connection);
-                 command.Parameters.AddWithValue("@email", email);
-
-                 connection.Open();
-                 int count = (int)command.ExecuteScalar();
-                 connection.Close();
-
-                 return count > 0;
-             }
-         }
-
-         private void StoreTokenInDatabase(string email, string token)
-         {
-             string connectionString = ConfigurationManager.ConnectionStrings["VoiceVanguardDB"].ConnectionString;
-             string query = "INSERT INTO passwordResetTokens (email, token, expiryDate) VALUES (@email, @token, DATEADD(day, 1, GETDATE()))";
-
-             using (SqlConnection connection = new SqlConnection(connectionString))
-             {
-                 SqlCommand command = new SqlCommand(query, connection);
-                 command.Parameters.AddWithValue("@email", email);
-                 command.Parameters.AddWithValue("@token",token);
-
-                 connection.Open();
-                 command.ExecuteNonQuery();
-                 connection.Close();
-             }
-         }
-
-         private bool SendResetEmail(string email, string resetLink)
-         {
-             string senderEmail = "voicevanguardofficial@gmail.com";
-             string appPassword = "ytwzeqnusfhdaxoz";
-
-             MailMessage message = new MailMessage(senderEmail, email);
-             message.Subject = "Password Reset Request";
-             message.Body = $"Please click the following link to reset your password: {resetLink}";
-             message.IsBodyHtml = true;
-
-             SmtpClient client = new SmtpClient("smtp.gmail.com", 465);
-             client.EnableSsl = false;
-             client.UseDefaultCredentials = false;
-             client.Credentials = new NetworkCredential(senderEmail, appPassword);
-
-             try
-             {
-                 client.Send(message);
-                 return true;
-             }
-             catch (Exception ex)
-             {
-                 // Log the error message
-                 System.Diagnostics.Debug.WriteLine($"Error sending email: {ex.Message}");
-
-                 // Display an alert message using JavaScript
-                 string errorMessage = $"Error sending email: {ex.Message.Replace("'", "\\'")}";
-                 string script = $"<script>alert('{errorMessage}');</script>";
-                 ClientScript.RegisterStartupScript(this.GetType(), "ErrorAlert", script);
-
-                 return false;
-             }
-         }*/
     }
 }
